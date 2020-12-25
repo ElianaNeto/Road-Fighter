22f09af8af67aed4c8ef56f7a8048b12bce183c5
Eliana Neto <70918654+ElianaNeto@users.noreply.github.com>
2020-12-25 12:43:16 +0100

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -12,9 +12,5 @@ public class GameFrame extends  JFrame
          this.pack();
          this.setVisible(true);
          this.setLocationRelativeTo(null);
-         //this.setSize(400,600);
-         
-         
-    }
-    
+    } 
 }
